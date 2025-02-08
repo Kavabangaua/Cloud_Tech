2261ac31c546261f44f9095bf00ccf6da2a9d333
Pavlenko Oleksii <apavlenko005@gmail.com>
2025-02-09 00:44:21 +0200

    connect to azure

--- MyMvcApp/Program.cs
+++ MyMvcApp/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddApplicationInsightsTelemetry();
 
 var app = builder.Build();
 
@@ -25,3 +26,4 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
